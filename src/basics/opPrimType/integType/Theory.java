d4f2eac895346fb9fea60f89051bcfb4c5954b69
loicbaillif <larmedeciel@gmail.com>
2025-04-09 15:51:39 +0100

    JavaSpring/intTypeOper/th/v1 declare 2 int variables

--- src/basics/opPrimType/integType/Theory.java
+++ src/basics/opPrimType/integType/Theory.java
@@ -7,5 +7,10 @@ import formatting.PrintTitle;
 public class Theory {
     public static void main(String[] args) {
         PrintTitle.printTitleLv1("Integer types and operations");
+
+        PrintTitle.printTitleLv2("Integer types: the basics");
+        int two = 2;
+        int ten = 10;
+
     }
 }
