dca922111396482f1a7565d03b7cc0ef073f792c
loicbaillif <larmedeciel@gmail.com>
2025-04-09 17:43:55 +0100

    JavaSpring/string/th/init String - Theory

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,10 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hyperskill - Java Backend Developer (Spring Boot)");
 
-        basics.opPrimType.chars.Theory.main(null);
+        // basics.opPrimType.chars.Theory.main(null);
         // basics.opPrimType.incDec.Theory.main(null);
         // basics.opPrimType.integType.Theory.main(null);
         // basics.simpleProg.readScanner.Theory.main(null);
+        basics.strings.stringType.Theory.main(null);
     }
 }
---
+++ src/basics/strings/stringType/Theory.java
@@ -0,0 +1,14 @@
+package basics.strings.stringType;
+
+// https://hyperskill.org/learn/step/3523
+
+import formatting.PrintTitle;
+
+public class Theory {
+    public static void main(String[] args) {
+        PrintTitle.printTitleLv1("String - Theory");
+
+
+        PrintTitle.printTitleLv1("End of Theory");
+    }
+}
