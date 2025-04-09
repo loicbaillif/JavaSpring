af3579baebe209f63fa4bc5ab6e029203bfd9a10
loicbaillif <larmedeciel@gmail.com>
2025-04-09 14:44:32 +0100

    JavaSpring/formatting/v1 print fill Lines

--- src/formatting/PrintTitle.java
+++ src/formatting/PrintTitle.java
@@ -4,5 +4,17 @@ public class PrintTitle {
     public static void printTitleLv1(String title) {
         // variables
         int outputWidth = 80;
+        char fillChar = '*';
+
+        // Print
+        for (int i = 0; i < outputWidth; i++) {
+            System.out.print(fillChar);
+        }
+        System.out.println();
+
+        for (int i = 0; i < outputWidth; i++) {
+            System.out.print("*");
+        }
+        System.out.println();
     }
 }
