6ff82bd0e4ccf35b348a5a7e6775eaab12ddb274
loicbaillif <larmedeciel@gmail.com>
2025-04-09 17:00:15 +0100

    JavaSpring/chars/th/v1 some Chars

--- src/basics/opPrimType/chars/Theory.java
+++ src/basics/opPrimType/chars/Theory.java
@@ -8,6 +8,12 @@ public class Theory {
     public static void main(String[] args) {
         PrintTitle.printTitleLv1("Characters");
 
+        char lowerCaseChar = 'g';
+        char upperCaseChar = 'J';
+        char numberChar = '3';
+        char spaceChar = ' ';
+        char dollarChar = '$';
+
         PrintTitle.printTitleLv1("End of Theory");
     }
 }
