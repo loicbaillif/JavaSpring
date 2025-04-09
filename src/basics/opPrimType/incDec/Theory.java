59601db88cb616632633761b1466b383c950ca6e
loicbaillif <larmedeciel@gmail.com>
2025-04-09 16:42:37 +0100

    JavaSpring/incDec/th/init Increment & decrement

---
+++ src/basics/opPrimType/incDec/Theory.java
@@ -0,0 +1,14 @@
+package basics.opPrimType.incDec;
+
+// https://hyperskill.org/learn/step/5008
+
+import formatting.PrintTitle;
+
+public class Theory {
+    public static void main(String[] args) {
+        PrintTitle.printTitleLv1("Increment and decrement");
+
+
+        PrintTitle.printTitleLv2("End of Theory");
+    }
+}
