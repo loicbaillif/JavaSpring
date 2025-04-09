aaecc90e5037ceca8282a39488ac26dce3ef4acc
loicbaillif <larmedeciel@gmail.com>
2025-04-09 13:43:03 +0100

    JavaSpring/scanner/th/init Reading user's input with Scanner

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hyperskill - Java Backend Developer (Spring Boot)");
+
+        basics.simpleProg.readScanner.Theory.main(null);
     }
 }
---
+++ src/basics/simpleProg/readScanner/Theory.java
@@ -0,0 +1,11 @@
+package basics.simpleProg.readScanner;
+
+// https://hyperskill.org/learn/step/9055
+
+public class Theory {
+    public static void main(String[] args) {
+        System.out.println("\n**********");
+        System.out.println("Reading user's input with Scanner");
+        System.out.println("**********\n");
+    }
+}
