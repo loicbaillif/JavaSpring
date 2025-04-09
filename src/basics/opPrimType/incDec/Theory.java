d587b44127ebe58490ac7ae126c23f335c9069d9
loicbaillif <larmedeciel@gmail.com>
2025-04-09 16:46:57 +0100

    JavaSpring/incDec/th/v1 ++ and --

--- src/basics/opPrimType/incDec/Theory.java
+++ src/basics/opPrimType/incDec/Theory.java
@@ -8,7 +8,14 @@ public class Theory {
     public static void main(String[] args) {
         PrintTitle.printTitleLv1("Increment and decrement");
 
+        PrintTitle.printTitleLv2("Using ++ and -- in Java");
+        int n = 10;
+        System.out.println("n = " + n);
+        n++;
+        System.out.println("n++ ==> n = " + n);
+        n--;
+        System.out.println("n-- ==> n = " + n);
 
-        PrintTitle.printTitleLv2("End of Theory");
+        PrintTitle.printTitleLv1("End of Theory");
     }
 }
