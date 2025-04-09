e5f704b95487639f0eef01e81ce9714fefe18a7b
loicbaillif <larmedeciel@gmail.com>
2025-04-09 13:44:12 +0100

    JavaSpring/scanner/th/v1 Open & Close scanner

--- src/basics/simpleProg/readScanner/Theory.java
+++ src/basics/simpleProg/readScanner/Theory.java
@@ -2,10 +2,17 @@ package basics.simpleProg.readScanner;
 
 // https://hyperskill.org/learn/step/9055
 
+import java.util.Scanner;
+
 public class Theory {
     public static void main(String[] args) {
         System.out.println("\n**********");
         System.out.println("Reading user's input with Scanner");
         System.out.println("**********\n");
+
+        Scanner scanner = new Scanner(System.in);
+
+
+        scanner.close();
     }
 }
