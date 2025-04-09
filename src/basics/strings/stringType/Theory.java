98af0776652a9d884c8f9d424f0123ca7045e88c
loicbaillif <larmedeciel@gmail.com>
2025-04-09 17:51:45 +0100

    JavaSpring/string/th/v1 Some strings

--- src/basics/strings/stringType/Theory.java
+++ src/basics/strings/stringType/Theory.java
@@ -8,6 +8,13 @@ public class Theory {
     public static void main(String[] args) {
         PrintTitle.printTitleLv1("String - Theory");
 
+        PrintTitle.printTitleLv2("The String type");
+        String someString1 = "This is a String";
+        System.out.println(someString1);
+        String someString2 = "This\nis\nmultiline\nstring";
+        System.out.println(someString2);
+        String someString3 = null;
+        System.out.println(someString3);
 
         PrintTitle.printTitleLv1("End of Theory");
     }
