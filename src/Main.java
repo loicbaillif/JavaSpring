abf1f4a4da9438e50741e7fed313051fc7887c62
loicbaillif <larmedeciel@gmail.com>
2025-04-09 16:58:21 +0100

    JavaSpring/chars/th/init Characters - init

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,8 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hyperskill - Java Backend Developer (Spring Boot)");
 
-        basics.opPrimType.incDec.Theory.main(null);
+        basics.opPrimType.chars.Theory.main(null);
+        // basics.opPrimType.incDec.Theory.main(null);
         // basics.opPrimType.integType.Theory.main(null);
         // basics.simpleProg.readScanner.Theory.main(null);
     }
---
+++ src/basics/opPrimType/chars/Theory.java
@@ -0,0 +1,14 @@
+package basics.opPrimType.chars;
+
+// https://hyperskill.org/learn/step/3514
+
+import formatting.PrintTitle;
+
+public class Theory {
+    public static void main(String[] args) {
+        PrintTitle.printTitleLv1("Characters");
+
+        PrintTitle.printTitleLv1("End of Theory");
+    }
+}
+
