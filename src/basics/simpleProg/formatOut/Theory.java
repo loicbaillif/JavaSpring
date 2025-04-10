d8ed66a45f48f42c5ddf0d7750cc252501071f5c
loicbaillif <larmedeciel@gmail.com>
2025-04-10 15:22:13 +0100

    JavaSpring/formatOut/th/v1 First example

--- src/basics/simpleProg/formatOut/Theory.java
+++ src/basics/simpleProg/formatOut/Theory.java
@@ -8,6 +8,8 @@ public class Theory {
     public static void main(String[] args) {
         PrintTitle.printTitleLv1("Formatted Output - Theory");
 
+        PrintTitle.printTitleLv2("printf() introduction");
+        System.out.printf("\tMy name is %s.\n\tI was born in %d.", "Paul", 1977);
 
         PrintTitle.printTitleLv1("End of Theory");
     }
