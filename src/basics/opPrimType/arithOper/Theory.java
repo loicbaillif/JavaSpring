ecaf9cf6fcbda42cb0a215daae55bb5414691704
loicbaillif <larmedeciel@gmail.com>
2025-04-11 12:25:23 +0100

    JavaSpring/arithOper/th/v1 Addition & subtraction

--- src/basics/opPrimType/arithOper/Theory.java
+++ src/basics/opPrimType/arithOper/Theory.java
@@ -6,6 +6,13 @@ public class Theory {
     public static void main(String[] args) {
         PrintTitle.printTitleLv1("Arithmetic Operations - Theory");
 
+        PrintTitle.printTitleLv2("Binary Arithmetic operators");
+        System.out.println(12 + 34);
+        System.out.println(35 + 753);
+
+        System.out.println(34 - 12);
+        System.out.println(35 - 753);
+
 
         PrintTitle.printTitleLv1("End of theory");
     }
