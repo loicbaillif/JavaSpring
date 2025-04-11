8673bccae7851da98a7d67e8d5e6fd2f3eca0b56
loicbaillif <larmedeciel@gmail.com>
2025-04-11 10:50:16 +0100

    JavaSpring/lastPencil/st1/init Last Pencil project - stage 1/5

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,10 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hyperskill - Java Backend Developer (Spring Boot)");
 
+        // Projects
+        projects.lastPencil.Stage1.main(null);
+
+        // Courses
         // basics.opPrimType.chars.Theory.main(null);
         // basics.opPrimType.incDec.Theory.main(null);
         // basics.opPrimType.integType.Theory.main(null);
---
+++ src/projects/lastPencil/Stage1.java
@@ -0,0 +1,9 @@
+package projects.lastPencil;
+
+import formatting.PrintTitle;
+
+public class Stage1 {
+    public static void main(String[] args) {
+        PrintTitle.printTitleLv1("Last Pencil - Stage 1/5");
+    }
+}
