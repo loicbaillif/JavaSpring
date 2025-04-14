312ece97c6831490ff56d72c2fb0716bae275ecf
loicbaillif <larmedeciel@gmail.com>
2025-04-14 20:21:55 +0100

    JavaSpring/floatPoint/th/v1 Declare doubles

--- src/basics/opPrimType/floatPoint/Theory.java
+++ src/basics/opPrimType/floatPoint/Theory.java
@@ -8,6 +8,12 @@ public class Theory {
     public static void main(String[] args) {
         PrintTitle.printTitleLv1("Floating-point types");
 
+        PrintTitle.printTitleLv2("Declaring variables and assigning values");
+        double zero = 0.0;
+        double one = 1.0;
+        double someNegDouble = -1.23;
+        double pi = 3.141;
+
 
         PrintTitle.printTitleLv1("End of theory");
     }
