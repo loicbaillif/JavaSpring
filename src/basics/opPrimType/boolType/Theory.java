4bf8972752dd6ec10ddcc0a1cf51b0ad1b08dbb2
loicbaillif <larmedeciel@gmail.com>
2025-04-15 20:01:25 +0100

    JavaSpring/boolType/th/v1 Boolean Types

--- src/basics/opPrimType/boolType/Theory.java
+++ src/basics/opPrimType/boolType/Theory.java
@@ -8,7 +8,11 @@ public class Theory {
     public static void main(String[] args) {
         PrintTitle.printTitleLv1("Boolean type and operations - Theory");
 
-
+        PrintTitle.printTitleLv2("Boolean Types");
+        boolean open = true;
+        boolean close = false;
+        System.out.println("open: " + open);
+        System.out.println("close: " + close);
 
         PrintTitle.printTitleLv1("End of Theory");
     }
