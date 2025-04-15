70e841b2eccacf9f7efcb759a36213b6ebfb3795
loicbaillif <larmedeciel@gmail.com>
2025-04-15 21:51:38 +0100

    JavaSpring/conditStat/th/v1 single if case

--- src/basics/controlFlow/condStatement/Theory.java
+++ src/basics/controlFlow/condStatement/Theory.java
@@ -4,11 +4,21 @@ package basics.controlFlow.condStatement;
 
 import formatting.PrintTitle;
 
+import java.util.Scanner;
+
 public class Theory {
     public static void main(String[] args) {
         PrintTitle.printTitleLv1("Conditional statement - Theory");
 
+        PrintTitle.printTitleLv2("Single if-case");
+        Scanner scanner = new Scanner(System.in);
+        int user_age = scanner.nextInt();
+        if (user_age < 18) {
+            System.out.println("You are less than 18 years old");
+        }
 
+
+        scanner.close();
 
         PrintTitle.printTitleLv1("End of Theory");
     }
