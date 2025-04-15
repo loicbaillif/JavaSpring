e4379b1c76267b7192644ad38059ef3eff4bb223
loicbaillif <larmedeciel@gmail.com>
2025-04-15 21:38:50 +0100

    JavaSpring/comparValues/th/v1 List of operators

--- src/basics/opPrimType/comparValues/Theory.java
+++ src/basics/opPrimType/comparValues/Theory.java
@@ -8,7 +8,13 @@ public class Theory {
     public static void main(String[] args) {
         PrintTitle.printTitleLv1("Comparing values. Relational operators - Theory");
 
-
+        PrintTitle.printTitleLv2("List of relational operators");
+        System.out.println("\t== (equal to)");
+        System.out.println("\t!= (not equal to)");
+        System.out.println("\t> (greater than)");
+        System.out.println("\t>= (greater than or equal to)");
+        System.out.println("\t< (less than)");
+        System.out.println("\t<= (less than or equal to)");
 
         PrintTitle.printTitleLv1("End of Theory");
     }
