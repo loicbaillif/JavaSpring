8e8b3ec65534aee4d59b0f2d40d958bc94a8a554
loicbaillif <larmedeciel@gmail.com>
2025-04-15 21:49:32 +0100

    JavaSpring/conditStat/th/init Conditional Statement

--- src/Main.java
+++ src/Main.java
@@ -6,13 +6,16 @@ public class Main {
         // projects.lastPencil.Stage1.main(null);
 
         // Courses
+        basics.controlFlow.condStatement.Theory.main(null);
+
         // basics.opPrimType.arithOper.Theory.main(null);
         // basics.opPrimType.boolType.Theory.main(null);
         // basics.opPrimType.chars.Theory.main(null);
-        basics.opPrimType.comparValues.Theory.main(null);
+        // basics.opPrimType.comparValues.Theory.main(null);
         // basics.opPrimType.floatPoint.Theory.main(null);
         // basics.opPrimType.incDec.Theory.main(null);
         // basics.opPrimType.integType.Theory.main(null);
+
         // basics.simpleProg.formatOut.Theory.main(null);
         // basics.simpleProg.readScanner.Theory.main(null);
         // basics.strings.stringType.Theory.main(null);
---
+++ src/basics/controlFlow/condStatement/Theory.java
@@ -0,0 +1,15 @@
+package basics.controlFlow.condStatement;
+
+// https://hyperskill.org/learn/step/3503
+
+import formatting.PrintTitle;
+
+public class Theory {
+    public static void main(String[] args) {
+        PrintTitle.printTitleLv1("Conditional statement - Theory");
+
+
+
+        PrintTitle.printTitleLv1("End of Theory");
+    }
+}
