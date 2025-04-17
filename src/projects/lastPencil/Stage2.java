84ef418e7146fe5c7f656e261499e1a6660edb05
loicbaillif <larmedeciel@gmail.com>
2025-04-17 20:15:33 +0100

    JavaSpring/lastPencil/st2/v1 Declare variables

--- src/projects/lastPencil/Stage2.java
+++ src/projects/lastPencil/Stage2.java
@@ -4,12 +4,18 @@ package projects.lastPencil;
 
 import formatting.PrintTitle;
 
+import java.util.Scanner;
+
 public class Stage2 {
     public static void main(String[] args) {
         PrintTitle pt = new PrintTitle();
         pt.printTitleLv1("Last Pencil - Stage 2/5");
 
-
+        // Variables
+        Scanner sc = new Scanner(System.in);
+        int nbPencils = sc.nextInt();
+        String player1 = "Josephine";
+        String player2 = "Jane";
 
         pt.printTitleLv1("End of stage 2");
     }
