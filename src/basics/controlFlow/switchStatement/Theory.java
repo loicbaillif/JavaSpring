d360075a131c0c641167b2882ed512c1ca0555f4
loicbaillif <larmedeciel@gmail.com>
2025-04-18 16:08:47 +0100

    JavaSpring/switch/th/init Switch Statement - Theory

---
+++ src/basics/controlFlow/switchStatement/Theory.java
@@ -0,0 +1,15 @@
+package basics.controlFlow.switchStatement;
+
+// https://hyperskill.org/learn/step/3504
+
+import formatting.PrintTitle;
+
+public class Theory {
+    public static void main(String[] args) {
+        PrintTitle.printTitleLv1("Switch Statement - Theory");
+
+
+
+        PrintTitle.printTitleLv1("End of theory");
+    }
+}
