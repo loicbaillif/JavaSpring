8a8ec8d79775e98398e8b0a0dceb9cba663337a5
loicbaillif <larmedeciel@gmail.com>
2025-04-18 17:36:45 +0100

    JavaSpring/array/th/v1 Declaration

--- src/workWithData/arrays/arrayType/Theory.java
+++ src/workWithData/arrays/arrayType/Theory.java
@@ -8,7 +8,8 @@ public class Theory {
     public static void main(String[] args) {
         PrintTitle.printTitleLv1("Array - Theory");
 
-
+        PrintTitle.printTitleLv2("Declaration");
+        int[] array; // Declaration
 
         PrintTitle.printTitleLv1("End of theory");
     }
