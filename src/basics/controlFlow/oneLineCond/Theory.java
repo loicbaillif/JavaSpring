435f7a09a3c05460a8c08157e574f01804a4ee83
loicbaillif <larmedeciel@gmail.com>
2025-04-18 12:15:12 +0100

    JavaSpring/oneLineCond/th/v1 first usage

--- src/basics/controlFlow/oneLineCond/Theory.java
+++ src/basics/controlFlow/oneLineCond/Theory.java
@@ -4,11 +4,17 @@ package basics.controlFlow.oneLineCond;
 
 import formatting.PrintTitle;
 
+import java.util.Scanner;
+
 public class Theory {
     public static void main(String[] args) {
         PrintTitle.printTitleLv1("One-line condition with ternary operator");
 
-
+        PrintTitle.printTitleLv2("First usage");
+        Scanner scanner = new Scanner(System.in);
+        int userAge = scanner.nextInt();
+        String greetings = userAge > 18? "Welcome dear user" : "Yo user!";
+        System.out.println(greetings);
 
         PrintTitle.printTitleLv1("End of theory");
     }
