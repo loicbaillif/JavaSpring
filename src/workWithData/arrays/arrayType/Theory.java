7d161524510df84e0235c121d32a921677f3e8a0
loicbaillif <larmedeciel@gmail.com>
2025-04-18 21:02:33 +0100

    JavaSpring/array/th/v6 Accessing elements

--- src/workWithData/arrays/arrayType/Theory.java
+++ src/workWithData/arrays/arrayType/Theory.java
@@ -12,6 +12,9 @@ public class Theory {
 
         PrintTitle.printTitleLv2("Declaration");
         int[] array; // Declaration
+        array = new int[10];
+        array[3] = 9;
+        System.out.println(Arrays.toString(array));
 
         PrintTitle.printTitleLv2("Instanciation");
         int[] numbersArray = {10, 30, 5, 7, 9, 11, 13};
@@ -19,9 +22,11 @@ public class Theory {
         PrintTitle.printTitleLv2("Creating an array with new");
         int n = 10;
         int[] numbers2Array = new int[n];
+        System.out.println(numbers2Array.length);
 
         float[] floatArray;
         floatArray = new float[] { 1.2f, 2.3f, 3.4f, 4.5f, 5.6f, 6.7f, 7.8f };
+        System.out.println(Arrays.toString(floatArray));
 
         char[] charArray = new char[n];
         int index = 0;
@@ -31,6 +36,14 @@ public class Theory {
 
         PrintTitle.printTitleLv2("Array length");
         System.out.println(numbersArray.length);
+
+        PrintTitle.printTitleLv2("Accessing elements");
+        for (int i = 0; i < numbersArray.length; i++) {
+            System.out.printf("\t- index %d => array[i] = %d\n", i, numbersArray[i]);
+        }
+
+        byte test = 4;
+        System.out.println(numbersArray[test]);
 
         PrintTitle.printTitleLv1("End of theory");
     }
