d2d1f83ef0fcd4e065724f083958832af7ea562e
loicbaillif <larmedeciel@gmail.com>
2025-04-18 17:53:37 +0100

    JavaSpring/array/th/v4 Create array with "new"

--- src/workWithData/arrays/arrayType/Theory.java
+++ src/workWithData/arrays/arrayType/Theory.java
@@ -4,6 +4,8 @@ package workWithData.arrays.arrayType;
 
 import formatting.PrintTitle;
 
+import java.util.Arrays;
+
 public class Theory {
     public static void main(String[] args) {
         PrintTitle.printTitleLv1("Array - Theory");
@@ -12,11 +14,20 @@ public class Theory {
         int[] array; // Declaration
 
         PrintTitle.printTitleLv2("Instanciation");
-        int[] numbersArray = {1, 3, 5, 7, 9};
+        int[] numbersArray = {10, 30, 5, 7, 9, 11, 13};
 
         PrintTitle.printTitleLv2("Creating an array with new");
-        int n = 5;
+        int n = 10;
         int[] numbers2Array = new int[n];
+
+        float[] floatArray;
+        floatArray = new float[] { 1.2f, 2.3f, 3.4f, 4.5f, 5.6f, 6.7f, 7.8f };
+
+        char[] charArray = new char[n];
+        int index = 0;
+        Arrays.fill(charArray, 0, n / 2, (char) (10 * numbersArray[index++]));
+        Arrays.fill(charArray, n / 2, n, (char) (8 * numbersArray[index]));
+        System.out.println(Arrays.toString(charArray));
 
         PrintTitle.printTitleLv1("End of theory");
     }
