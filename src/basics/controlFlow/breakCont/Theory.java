f0ea6e6f6da463fa4ae97a82b4b72faf407726a2
loicbaillif <larmedeciel@gmail.com>
2025-04-18 15:44:37 +0100

    JavaSpring/breakCont/th/v1 break - 2 examples

--- src/basics/controlFlow/breakCont/Theory.java
+++ src/basics/controlFlow/breakCont/Theory.java
@@ -8,7 +8,25 @@ public class Theory {
     public static void main(String[] args) {
         PrintTitle.printTitleLv1("break & continue: branching - Theory");
 
+        PrintTitle.printTitleLv2("The break statement");
+        int i = -5;
+        while (true) {
+            if (i == 3) {
+                break;
+            }
+            System.out.printf("%d\n", i);
+            i++;
+        }
 
+        for (i = 0; i < 10; i++) {
+            for (int j = 0; j < 10; j++) {
+                System.out.printf("%d%d ", i, j);
+                if (j == i) {
+                    break;
+                }
+            }
+            System.out.println();
+        }
 
         PrintTitle.printTitleLv1("End of Theory");
     }
