68efb54805c7bfea888ae30d343d87499b153254
loicbaillif <larmedeciel@gmail.com>
2025-04-18 12:33:20 +0100

    JavaSpring/forLoop/th/v1 couple examples

--- src/basics/controlFlow/forLoop/Theory.java
+++ src/basics/controlFlow/forLoop/Theory.java
@@ -8,7 +8,15 @@ public class Theory {
     public static void main(String[] args) {
         PrintTitle.printTitleLv1("for loop - Theory");
 
+        int nbRepetitions = 5;
+        for (int i = 0; i < nbRepetitions; i++) {
+            System.out.println("Repetition " + i);
+        }
 
+        int startNumber = 8;
+        for (int i = startNumber; i < startNumber + nbRepetitions; i++) {
+            System.out.printf("\tStart at %d - repetition %d - value = %d%n", startNumber, (i - startNumber), i);
+        }
 
         PrintTitle.printTitleLv1("End of theory");
     }
