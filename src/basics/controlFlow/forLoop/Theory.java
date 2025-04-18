13bac1d87df26f31db0b1800157cc4f2042d95ed
loicbaillif <larmedeciel@gmail.com>
2025-04-18 12:37:20 +0100

    JavaSpring/forLoop/th/v2 skipping parts in declaration

--- src/basics/controlFlow/forLoop/Theory.java
+++ src/basics/controlFlow/forLoop/Theory.java
@@ -4,10 +4,13 @@ package basics.controlFlow.forLoop;
 
 import formatting.PrintTitle;
 
+import java.util.Scanner;
+
 public class Theory {
     public static void main(String[] args) {
         PrintTitle.printTitleLv1("for loop - Theory");
 
+        PrintTitle.printTitleLv2("Basic syntax");
         int nbRepetitions = 5;
         for (int i = 0; i < nbRepetitions; i++) {
             System.out.println("Repetition " + i);
@@ -17,6 +20,20 @@ public class Theory {
         for (int i = startNumber; i < startNumber + nbRepetitions; i++) {
             System.out.printf("\tStart at %d - repetition %d - value = %d%n", startNumber, (i - startNumber), i);
         }
+
+        PrintTitle.printTitleLv2("Skipping parts");
+
+        for (; nbRepetitions > 0; nbRepetitions--) {
+            System.out.println("Skipping the initial declaration");
+        }
+        Scanner scanner = new Scanner(System.in);
+        for (;;) {  // Infinite loop
+            if ("exit".equals(scanner.nextLine())) {
+                break;
+            }
+        }
+
+        scanner.close();
 
         PrintTitle.printTitleLv1("End of theory");
     }
